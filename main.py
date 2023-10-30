fc67b7f1889b3108af4bab1b075902fa5a66fa4b
Chris Strong <strongdev6@gmail.com>
2023-10-30 10:06:02 -0500

    Changed get movie by id an index instead of a loop and changed releaseYear to releaseDate

--- main.py
+++ main.py
@@ -8,26 +8,31 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+
 movies = []
+
 
 # get all movies listing
 @app.get("/movies")
 async def get_movie():
     return {"movie": movies}
 
+
 # get a single movie listing
 @app.get("/movies/{movie_id}")
 async def get_movie(movie_id: int):
-    for movie in movies:
-        if movie.id == movie_id:
-            return {"movie": movie}
-    return {"message": "No movie found"}
+    try:
+        return {"movie": movies[movies.index(movie_id)]}
+    except:
+        return {"message": "No movie found"}
+
 
 # create a movie listing
 @app.post("/movies")
 async def create_movie(movie: Movies):
     movies.append(movie)
     return {"message": "Movie was created"}
+
 
 # delete a movie listing
 @app.delete("/movies/{movie_id}")
@@ -37,4 +42,3 @@ async def delete_movie(movie_id: int):
             movies.remove(movie)
             return {"message": "Movie was deleted"}
     return {"message": "No movie found"}
-
--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+from datetime import date
 from pydantic import BaseModel, Field
 import uuid
 
@@ -5,7 +6,7 @@ import uuid
 class Movies(BaseModel):
     id: uuid.UUID = Field(default_factory=uuid.uuid4)
     title: str
-    releaseYear: int
+    releaseDate: date
     director: str
     rating: int
     genre: str
