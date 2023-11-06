534a1deef69c7bf38cc6c916cccf2ac2c2e467de
Chris Strong <strongdev6@gmail.com>
2023-11-06 10:29:29 -0600

    Updated endpoints and added update endpoint

--- main.py
+++ main.py
@@ -1,46 +1,55 @@
 import uuid
-
 from fastapi import FastAPI
-from model import Movies
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+class Movies(BaseModel):
+    id: int = Field(default_factory=lambda: uuid.uuid4().int)  # Generates a unique ID if not provided
+    title: str
 
 
-movies = []
+movies = []  # This will be a list of Movies instances
 
 
-# get all movies listing
 @app.get("/movies")
-async def get_movie():
-    return {"movie": movies}
+async def get_movies():
+    return {"movies": movies}
 
 
-# get a single movie listing
 @app.get("/movies/{movie_id}")
-async def get_movie(movie_id: uuid.UUID):
-    try:
-        return {"movie": movies[movies.index(movie_id)]}
-    except:
-        return {"message": "No movie found"}
+async def get_movie(movie_id: int):
+    movie = next((movie for movie in movies if movie.id == movie_id), None)
+    if movie:
+        return {"movie": movie}
+    else:
+        return {"message": "No movie found"}, 404
 
 
-# create a movie listing
 @app.post("/movies")
 async def create_movie(movie: Movies):
     movies.append(movie)
-    return {"message": f"Movie titled '{movie.title}' created"}
+    return {"message": f"Movie titled '{movie.title}' created"}, 201
 
 
-# delete a movie listing
 @app.delete("/movies/{movie_id}")
-async def delete_movie(movie_id: uuid.UUID):
-    for movie in movies:
+async def delete_movie(movie_id: int):
+    movie = next((movie for movie in movies if movie.id == movie_id), None)
+    if movie:
+        movies.remove(movie)
+        return {"message": f"Movie titled '{movie.title}' was deleted"}
+    else:
+        return {"message": "No movie found"}, 404
+
+
+@app.put("/movies/{movie_id}")
+async def update_movie(movie_id: int, update_data: Movies):
+    # Find the movie by its ID and update it with the provided data
+    for idx, movie in enumerate(movies):
         if movie.id == movie_id:
-            movies.remove(movie)
-            return {"message": f"Movie title '{movie.title}' was deleted"}
-    return {"message": "No movie found"}
+            updated_movie_data = movie.copy(update=update_data.model_dump(exclude_unset=True))
+            movies[idx] = updated_movie_data
+            return {"message": f"Movie with id '{movie_id}' has been updated", "movie": updated_movie_data}
+        else:
+            return {"message": "No movie found"}, 404
